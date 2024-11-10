14d5ae6ead87d070601ed8be84ebc0c3b48b4f19
Thomas Bonk <thomas@meandmymac.de>
2024-11-10 18:37:38 +0100

    Initial Commit

---
+++ PiControl/PiControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiControlApp.swift
+//  PiControl
+//
+//  Created by Thomas Bonk on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct PiControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
